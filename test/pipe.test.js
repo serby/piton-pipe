cb491ab905b9936c8d09883ec7e929d1adbd75c4
Paul Serby <paul.serby@clock.co.uk>
2011-12-17 13:46:28 +0000

    Refactor to mocha and adding tests for remove and clear

--- test/pipe.test.js
+++ test/pipe.test.js
@@ -6,41 +6,105 @@ function additionProcess(value, callback) {
 	callback(null, value + 1);
 }
 
-module.exports = {
-	'add without a function fails': function() {
-		var pipe = Pipe.createPipe();
-		assert.throws(function() {
-			pipe.add();
-		}, /TypeError/);
-	},
-	'add is chainable': function() {
-		var pipe = Pipe.createPipe();
-		assert.strictEqual(pipe.add(additionProcess), pipe);
-	},
-	'run does nothing with an empty pipe': function() {
-		var pipe = Pipe.createPipe();
-		pipe.run(1, function(error, value) {
-			assert.eql(value, 1);
+describe('pipe', function() {
+
+	describe('#add()', function() {
+
+		it('without a function as a parameter fails', function() {
+			var pipe = Pipe.createPipe();
+			assert.throws(function() {
+				pipe.add();
+			}, /TypeError/);
 		});
-	},
-	'run processes correctly': function() {
 
-		var pipe = Pipe.createPipe()
-		.add(additionProcess)
-		.add(additionProcess);
-
-		pipe.run(1, function(error, value) {
-			assert.eql(value, 3);
+		it('is chainable', function() {
+			var pipe = Pipe.createPipe();
+			pipe.add(additionProcess).should.equal(pipe);
 		});
-	},
-	'remove is successful': function() {
+	});
 
-		var pipe = Pipe.createPipe()
-		.add(additionProcess)
-		.add(additionProcess);
+	describe('#run()', function() {
 
-		pipe.run(1, function(error, value) {
-			assert.eql(value, 3);
+		it('does nothing with an empty pipe', function(done) {
+			var pipe = Pipe.createPipe();
+			pipe.run(1, function(error, value) {
+				value.should.equal(1);
+				done();
+			});
 		});
-	}
-};
+
+		it('run processes correctly', function(done) {
+
+			var pipe = Pipe.createPipe()
+			.add(additionProcess)
+			.add(additionProcess);
+
+			pipe.run(1, function(error, value) {
+				value.should.equal(3);
+				done();
+			});
+
+		});
+
+	});
+
+	describe('#remove()', function() {
+
+		it('is successful', function(done) {
+
+			var functionToRemove = function(value, callback) {
+				callback(null, value + 10);
+			};
+
+			var pipe = Pipe.createPipe()
+			.add(additionProcess)
+			.add(functionToRemove);
+
+			pipe.remove(functionToRemove);
+
+			pipe.run(1, function(error, value) {
+				value.should.equal(2);
+				done();
+			});
+
+		});
+
+		it('throws error if process function not found', function() {
+
+			var pipe = Pipe.createPipe();
+			assert.throws(function() {
+				pipe.remove(additionProcess);
+			}, /RangeError: processor function not found in pipe/ );
+
+		});
+
+		it('is chainable', function() {
+			var pipe = Pipe.createPipe();
+
+			pipe
+				.add(additionProcess)
+				.remove(additionProcess).should.equal(pipe);
+		});
+	});
+
+	describe('#clear()', function() {
+
+		it('does not error with empty pipe', function() {
+
+			var pipe = Pipe.createPipe();
+
+			pipe.clear();
+		});
+
+		it('is chainable', function() {
+
+			var pipe = Pipe.createPipe();
+
+			pipe
+				.clear();
+
+		});
+
+	});
+
+});
